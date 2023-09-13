7c286c80f8c3a26d99253bf0a7c5d7d501251bc5
EC2 Default User <ec2-user@ip-172-31-45-187.ec2.internal>
2023-09-13 14:37:22 +0000

    Modified main.tf to just deploy a EC2 instance

--- main.tf
+++ main.tf
@@ -11,21 +11,3 @@ resource "aws_instance" "example" {
   }
 }
 
-# Define a security group to allow SSH access
-resource "aws_security_group" "example" {
-  name        = "example-ssh"
-  description = "Allow SSH traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH access from anywhere (for demonstration purposes)
-  }
-}
-
-# Associate the security group with the EC2 instance
-resource "aws_instance" "example" {
-  security_groups = [aws_security_group.example.name]
-}
-
